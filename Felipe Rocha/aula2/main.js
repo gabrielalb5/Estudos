010f202486c768c2f1aa581fddf5e8c67103c8b8
gabrielalb5 <gabrielalbino28@gmail.com>
2024-09-16 12:31:40 -0300

    aula 2

--- Felipe Rocha/aula2/main.js
+++ Felipe Rocha/aula2/main.js
@@ -1,3 +1,41 @@
-const message = "Hello world!"
+//Strings
+
+const message = 'Hello world!'
 
 alert(message)
+
+console.log(message.length);
+
+const firstName = 'Gabriel';
+const lastName = 'Albino';
+
+const names = "Felipe,João,Julia";
+
+console.log('Meu nome é '+firstName+' '+lastName);
+console.log(`Meu nome é ${firstName.toLowerCase()} ${lastName.toUpperCase()}`);
+console.log(names.split(","));
+
+//Numbers
+
+const number = 5;
+
+console.log(number)
+console.log(number+2)
+console.log(number-2)
+console.log(number/2)
+console.log(number*2)
+
+console.log(number.toString())
+console.log(typeof number.toString())
+
+//Booleans
+
+console.log(2 == 3) //false
+console.log(2 == 2) //true
+
+//Null & undefined
+
+const x = null;
+const y = undefined;
+console.log(x)
+console.log(y);
