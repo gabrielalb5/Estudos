358491c2ec6f244582ebfc8acf3fd713b12b72a4
gabrielalb5 <gabrielalbino28@gmail.com>
2024-09-19 15:24:21 -0300

    aula 13

---
+++ Felipe Rocha/aula13/main.js
@@ -0,0 +1,15 @@
+const items = document.querySelector('.items');
+console.log(items);
+
+const button = document.querySelector('.btn');
+
+//items.remove();
+
+//items.firstElementChild.remove();
+//items.lastElementChild.remove();
+
+//items.children[1].textContent = 'Item dois';
+//items.lastElementChild.innerHTML = "<h1>Hello World!</h1>";
+
+button.style.background = "red";
+button.style.color = "white";
